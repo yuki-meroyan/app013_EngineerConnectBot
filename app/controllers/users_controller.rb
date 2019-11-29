da685a64325d86e821e58081b3eedb2b60f82b6a
yuki_m <yakan.meroyan@gmail.com>
2019-11-30 01:09:20 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,21 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def followed
+    @title = "フォロー"
+    @user = User.find(params[:id])
+    @users = @user.followeds
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "フォロワー"
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
   end
 
 end
