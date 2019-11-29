942b07ff7ccf1de95818557a5e4f8ac6e94cd332
yuki_m <yakan.meroyan@gmail.com>
2019-11-30 01:09:25 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,22 @@ class User < ApplicationRecord
   has_many :seniors
   has_many :group_users, dependent: :destroy
   has_many :groups, through: :group_users
+
+  has_many :followed_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followeds, through: :followed_relationships
+
+  has_many :follower_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+  def followed?(other_user)
+    followed_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    followed_relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    followed_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
