82bf76daecca5d44d211e5d9775e569ce0a2a039
yuki_m <yakan.meroyan@gmail.com>
2019-11-30 23:35:47 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   resources :beginners
   resources :intermediates
   resources :seniors
-  resources :groups
+  resources :groups do
+    resources :group_messages
+  end
   resources :relationships, only: [:create, :destroy]
 end
