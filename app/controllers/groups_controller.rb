b9f5ba723bfa4883f6a44ea71b096341b414f78f
yuki_m <yakan.meroyan@gmail.com>
2019-11-30 23:35:24 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,7 +26,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+      redirect_to engineer_connect_bots_have_group_path(current_user.id), notice: 'グループを編集しました'
     else
       render :edit
     end
