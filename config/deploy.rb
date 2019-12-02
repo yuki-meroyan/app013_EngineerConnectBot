ef68a74e5f3be628f71ac34867691f315cd07770
yuki_m <yakan.meroyan@gmail.com>
2019-12-02 22:45:07 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ set :application, "app013_EngineerConnectBot"
 set :repo_url, "git@example.com:yuki-meroyan/app013_EngineerConnectBot.git"
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
