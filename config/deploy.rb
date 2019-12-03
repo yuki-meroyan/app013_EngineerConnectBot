d68176c38b8f0a5444eb4685de8371574985c16e
yuki_m <yakan.meroyan@gmail.com>
2019-12-03 22:35:23 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,10 @@ set :repo_url, "git@github.com:yuki-meroyan/app013_EngineerConnectBot.git"
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+# credentials.yml.enc用のシンボリックリンクを追加
+set :linked_files, %w{ config/credentials.yml.enc }
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
@@ -58,42 +62,40 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 # Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :unicorn_config_path, -> { "#{shared_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# # デプロイ処理が終わった後、Unicornを再起動するための記述
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#   task :restart do
-#     invoke 'unicorn:restart'
-#   end
-# end
-
-# credentials.yml.enc用のシンボリックリンクを追加
-set :linked_files, %w{ config/credentials.yml.enc }
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
-
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    # invoke 'unicorn:restart'
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
+    invoke 'unicorn:restart'
   end
-
-  desc 'upload credentials.yml.enc'
-  desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
-      upload!('config/master.key', "#{shared_path}/config/master.key")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
 end
+
+
+
+# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     # invoke 'unicorn:restart'
+#     invoke 'unicorn:stop'
+#     invoke 'unicorn:start'
+#   end
+
+#   desc 'upload credentials.yml.enc'
+#   desc 'upload master.key'
+#   task :upload do
+#     on roles(:app) do |host|
+#       if test "[ ! -d #{shared_path}/config ]"
+#         execute "mkdir -p #{shared_path}/config"
+#       end
+#       upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+#       upload!('config/master.key', "#{shared_path}/config/master.key")
+#     end
+#   end
+#   before :starting, 'deploy:upload'
+#   after :finishing, 'deploy:cleanup'
+# end
