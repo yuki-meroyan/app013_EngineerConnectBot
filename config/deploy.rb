91d2617af954ce44f0db9d027220c4eda5c2190c
yuki_m <yakan.meroyan@gmail.com>
2019-12-03 23:57:08 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,9 @@ set :repo_url, "git@github.com:yuki-meroyan/app013_EngineerConnectBot.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# credentials.yml.enc用のシンボリックリンクを追加
+set :linked_files, %w{ config/credentials.yml.enc }
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
@@ -62,43 +65,41 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#   task :restart do
-#     invoke 'unicorn:restart'
-#   end
-# end
-
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"
-}
-
-# credentials.yml.enc用のシンボリックリンクを追加
-set :linked_files, %w{ config/credentials.yml.enc }
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
-
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    # invoke 'unicorn:restart'
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
+    invoke 'unicorn:restart'
   end
-
-  desc 'upload credentials.yml.enc'
-  desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
-      upload!('config/master.key', "#{shared_path}/config/master.key")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
 end
+
+# set :default_env, {
+#   rbenv_root: "/usr/local/rbenv",
+#   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"
+# }
+
+
+
+# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     # invoke 'unicorn:restart'
+#     invoke 'unicorn:stop'
+#     invoke 'unicorn:start'
+#   end
+
+#   desc 'upload credentials.yml.enc'
+#   desc 'upload master.key'
+#   task :upload do
+#     on roles(:app) do |host|
+#       if test "[ ! -d #{shared_path}/config ]"
+#         execute "mkdir -p #{shared_path}/config"
+#       end
+#       upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+#       upload!('config/master.key', "#{shared_path}/config/master.key")
+#     end
+#   end
+#   before :starting, 'deploy:upload'
+#   after :finishing, 'deploy:cleanup'
+# end
