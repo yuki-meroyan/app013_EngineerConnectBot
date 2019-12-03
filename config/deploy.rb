7931bb08b93ae42a98db624fae5b3c38cc012938
yuki_m <yakan.meroyan@gmail.com>
2019-12-03 22:45:39 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,40 +62,40 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 # Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "/config/unicorn.rb" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
+# end
 
 
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
 # デプロイ処理が終わった後、Unicornを再起動するための記述
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#   task :restart do
-#     # invoke 'unicorn:restart'
-#     invoke 'unicorn:stop'
-#     invoke 'unicorn:start'
-#   end
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    # invoke 'unicorn:restart'
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
 
-#   desc 'upload credentials.yml.enc'
-#   desc 'upload master.key'
-#   task :upload do
-#     on roles(:app) do |host|
-#       if test "[ ! -d #{shared_path}/config ]"
-#         execute "mkdir -p #{shared_path}/config"
-#       end
-#       upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
-#       upload!('config/master.key', "#{shared_path}/config/master.key")
-#     end
-#   end
-#   before :starting, 'deploy:upload'
-#   after :finishing, 'deploy:cleanup'
-# end
+  desc 'upload credentials.yml.enc'
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
