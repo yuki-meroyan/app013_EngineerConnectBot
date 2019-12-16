7be6dfdc70d6f71ee558fba69dbcc5004ba3d184
yuki_m <yakan.meroyan@gmail.com>
2019-12-17 00:24:08 +0900

    Update message.js

--- app/assets/javascripts/group/message.js
+++ app/assets/javascripts/group/message.js
@@ -5,47 +5,44 @@ $(function(){
                                             ${group_message.text}
                                           </div>`: ""
     var getImageTag = group_message.image.url? `<img class="group__message__image" src=${group_message.image.url}>`: ""
-    console.log(userImageTag);
-    console.log(getTextTag);
-    console.log(getTextTag);
-    // if (group_message.text != ""){
-    //   var htmlText = `<div class="group__message__box" data-message_id=${group_message.id}>
-    //                     <div class="group__message__contents position-right">
-    //                       <div class="group__message__content position-right">
-    //                         ${getTextTag}
-    //                       </div>
-    //                     </div>
-    //                     <div class="group__message__user">
-    //                       ${userImageTag}
-    //                       <p class="group__message__user__name">
-    //                         ${group_message.user_name}
-    //                       </p>
-    //                       <p class="group__message__date">
-    //                         ${group_message.created_at}
-    //                       </p>
-    //                     </div>
-    //                   </div>`
-    //   $('.group__message').append(htmlText);
-    // }
-    // if (group_message.image.url != ""){
-    //   var htmlImage = `<div class="group__message__box" data-message_id=${group_message.id}>
-    //                     <div class="group__message__contents position-right">
-    //                       <div class="group__message__content position-right">
-    //                       ${getImageTag}
-    //                       </div>
-    //                     </div>
-    //                     <div class="group__message__user">
-    //                       ${userImageTag}
-    //                       <p class="group__message__user__name">
-    //                         ${group_message.user_name}
-    //                       </p>
-    //                       <p class="group__message__date">
-    //                         ${group_message.created_at}
-    //                       </p>
-    //                     </div>
-    //                   </div>`
-    //   $('.group__message').append(htmlImage);
-    // }
+    if (group_message.text != null){
+      var htmlText = `<div class="group__message__box" data-message_id=${group_message.id}>
+                        <div class="group__message__contents position-right">
+                          <div class="group__message__content position-right">
+                            ${getTextTag}
+                          </div>
+                        </div>
+                        <div class="group__message__user">
+                          ${userImageTag}
+                          <p class="group__message__user__name">
+                            ${group_message.user_name}
+                          </p>
+                          <p class="group__message__date">
+                            ${group_message.created_at}
+                          </p>
+                        </div>
+                      </div>`
+      $('#group__message').append(htmlText);
+    }
+    if (group_message.image.url != null){
+      var htmlImage = `<div class="group__message__box" data-message_id=${group_message.id}>
+                        <div class="group__message__contents position-right">
+                          <div class="group__message__content position-right">
+                          ${getImageTag}
+                          </div>
+                        </div>
+                        <div class="group__message__user">
+                          ${userImageTag}
+                          <p class="group__message__user__name">
+                            ${group_message.user_name}
+                          </p>
+                          <p class="group__message__date">
+                            ${group_message.created_at}
+                          </p>
+                        </div>
+                      </div>`
+      $('#group__message').append(htmlImage);
+    }
   }
 
   function buildMessageHTML(message){
@@ -64,7 +61,7 @@ $(function(){
                     ${message.created_at}
                   </div>
                 </div>
-                <div class="message__lower"> 
+                <div class="message__lower">
                   ${getMessageContent}
                   ${getMessageImage}
                 </div>`
@@ -98,35 +95,35 @@ $(function(){
   })
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    if (location.pathname.search('groups/\[0-9]+\/group_messages') == 1 ){
-      last_message_id = $('.group__message__box:last').data('message_id');
-      var groupId = $('.group__main').data('group_id')
-      $.ajax({
-        //ルーティングで設定した通りのURLを指定
-        url: '/groups/' + groupId + '/api/group_messages' ,
-        //ルーティングで設定した通りhttpメソッドをgetに指定
-        type: 'get',
-        dataType: 'json',
-        //dataオプションでリクエストに値を含める
-        data: { id: last_message_id }
-      })
-      .done(function(messages) {
-        //追加するHTMLの入れ物を作る
-        var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(group_messages,function(index, group_message) {
-          //メッセージが入ったHTMLを取得
-          insertHTML = buildMessageHTML(group_message);
-          //メッセージを追加
-          $('.group__message').append(insertHTML);
-          $('.group__message').animate({ scrollTop: $('.group__message')[0].scrollHeight});
-        })
-      })
-      .fail(function() {
-        alert('error');
-      });
-    }
+    // //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    // if (location.pathname.search('groups/\[0-9]+\/group_messages') == 1 ){
+    //   last_message_id = $('.group__message__box:last').data('message_id');
+    //   var groupId = $('.group__main').data('group_id')
+    //   $.ajax({
+    //     //ルーティングで設定した通りのURLを指定
+    //     url: '/groups/' + groupId + '/api/group_messages' ,
+    //     //ルーティングで設定した通りhttpメソッドをgetに指定
+    //     type: 'get',
+    //     dataType: 'json',
+    //     //dataオプションでリクエストに値を含める
+    //     data: { id: last_message_id }
+    //   })
+    //   .done(function(messages) {
+    //     //追加するHTMLの入れ物を作る
+    //     var insertHTML = '';
+    //     //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+    //     $.each(group_messages,function(index, group_message) {
+    //       //メッセージが入ったHTMLを取得
+    //       insertHTML = buildMessageHTML(group_message);
+    //       //メッセージを追加
+    //       $('.group__message').append(insertHTML);
+    //       $('.group__message').animate({ scrollTop: $('.group__message')[0].scrollHeight});
+    //     })
+    //   })
+    //   .fail(function() {
+    //     alert('error');
+    //   });
+    // }
   };
 
   // setInterval(reloadMessages, 5000);
