6014fb141d419489c099ffe25779d572ca51e6ec
yuki_m <yakan.meroyan@gmail.com>
2019-12-18 00:42:48 +0900

    Update beginner_comments_controller.rb

--- app/controllers/beginner_comments_controller.rb
+++ app/controllers/beginner_comments_controller.rb
@@ -1,3 +1,30 @@
 class BeginnerCommentsController < ApplicationController
-  
+  before_action :comment_params, only: [:new, :create]
+  before_action :set_beginner, only: [:create]
+
+  def index
+  end
+
+  def create
+    @comment = @beginner.beginner_comments.new(comment_params)
+    if @comment.save
+      redirect_to beginner_path(comment_params[:beginner_id]), notice: 'コメントしました'
+      # respond_to do |format|
+      #   format.html { redirect_to blogtext_path(comment_params[:blogtext_id]) }
+      #   format.json
+      # end
+    else
+      redirect_to beginner_path(comment_params[:beginner_id]), notice: "コメントを失敗しました"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:beginner_comment).permit(:comment).merge(beginner_id: params[:beginner_id]).merge(user_id: current_user.id)
+  end
+
+  def set_beginner
+    binding.pry
+    @beginner = Beginner.find(params[:beginner_id])
+  end
 end
