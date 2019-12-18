0c592e285f8a266b63327393653e9a43749edcc1
yuki_m <yakan.meroyan@gmail.com>
2019-12-18 20:12:50 +0900

    Update 20191218105958_create_senior_comments.rb

--- db/migrate/20191218105958_create_senior_comments.rb
+++ db/migrate/20191218105958_create_senior_comments.rb
@@ -1,7 +1,9 @@
 class CreateSeniorComments < ActiveRecord::Migration[5.2]
   def change
     create_table :senior_comments do |t|
-
+      t.text       :comment       , null: false
+      t.references :user          , null: false, foreign_key: true
+      t.references :senior        , null: false, foreign_key: true
       t.timestamps
     end
   end
