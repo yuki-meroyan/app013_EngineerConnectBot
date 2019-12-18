70e4f942890f5cb18384b20ff11dc16cb222bd18
yuki_m <yakan.meroyan@gmail.com>
2019-12-18 20:12:47 +0900

    Update 20191218105924_create_intermediate_comments.rb

--- db/migrate/20191218105924_create_intermediate_comments.rb
+++ db/migrate/20191218105924_create_intermediate_comments.rb
@@ -1,7 +1,9 @@
 class CreateIntermediateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :intermediate_comments do |t|
-
+      t.text       :comment       , null: false
+      t.references :user          , null: false, foreign_key: true
+      t.references :intermediate  , null: false, foreign_key: true
       t.timestamps
     end
   end
