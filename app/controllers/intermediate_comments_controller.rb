c10364445b6e67603c14b016c9bf967378e0dc4f
yuki_m <yakan.meroyan@gmail.com>
2019-12-18 20:03:32 +0900

    Create intermediate_comments_controller.rb

---
+++ app/controllers/intermediate_comments_controller.rb
@@ -0,0 +1,25 @@
+class IntermediateCommentsController < ApplicationController
+  before_action :comment_params, only: [:new, :create]
+  before_action :set_intermediate, only: [:create]
+
+  def index
+  end
+
+  def create
+    @comment = @intermediate.intermediate_comments.new(comment_params)
+    if @comment.save
+      redirect_to intermediate_path(comment_params[:intermediate_id]), notice: 'コメントしました'
+    else
+      redirect_to intermediate_path(comment_params[:intermediate_id]), notice: "コメントを失敗しました"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:intermediate_comment).permit(:comment).merge(intermediate_id: params[:intermediate_id]).merge(user_id: current_user.id)
+  end
+
+  def set_intermediate
+    @intermediate = Beginner.find(params[:intermediate_id])
+  end
+end
