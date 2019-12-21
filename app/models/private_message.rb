b807c33f2165fa388a0e53f91c80714efc0981a4
yuki_m <yakan.meroyan@gmail.com>
2019-12-21 17:47:32 +0900

    Update private_message.rb

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -1,2 +1,7 @@
 class PrivateMessage < ApplicationRecord
+  belongs_to :user     , class_name: "User"
+  belongs_to :send_user, class_name: "User"
+
+  validates :send_user_id, presence: true
+  validates :text, presence: true
 end
