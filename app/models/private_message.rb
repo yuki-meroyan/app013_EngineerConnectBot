b83d125561cc7b49eab2f783a5967c1f5914863a
yuki_m <yakan.meroyan@gmail.com>
2019-12-24 00:36:34 +0900

    Update private_message.rb

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -5,4 +5,18 @@ class PrivateMessage < ApplicationRecord
   validates :user_id     , presence: true
   validates :send_user_id, presence: true
   validates :text        , presence: true
+
+  def un_read_message_check(user_id, send_message_user_id)
+    receive_user =PrivateMessage.where(user_id: send_message_user_id, send_user_id: user_id).count
+    receive_user_checked_message = PrivateMessage.where(user_id: send_message_user_id, send_user_id: user_id, read_check: 1).count
+    message_count = receive_user - receive_user_checked_message
+    if 0 == message_count
+    else
+      # 未読メッセージが無い際に未読バッジを表示させないため
+      message_count
+      # content_tag(:div, message_count, class: 'private__message__box--unReadCount')
+      # content_tag(:div, message_count, class: 'private__message__box--unReadCount')
+      # content_tag(:div, content_tag(:p, message_count), class: 'private__message__box--unReadCount')
+    end
+  end
 end
