5b8b12af21482a81ec6ba01407cb5bd7c9f5d7a5
yuki_m <yakan.meroyan@gmail.com>
2019-12-24 00:36:27 +0900

    Create private_message_list.js

---
+++ app/assets/javascripts/private_message/private_message_list.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('#private-from').on('click', function() {
+    $('.private-from-list').toggle("slow");
+  })
+  $('#private-to').on('click', function() {
+    $('.private-to-list').toggle("slow");
+  })
+})
