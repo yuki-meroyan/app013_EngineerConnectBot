025abf3765055b6a43822d1fd43f549611a53bea
yuki_m <yakan.meroyan@gmail.com>
2019-12-25 23:57:39 +0900

    Update private_message_list.js

--- app/assets/javascripts/private_message/private_message_list.js
+++ app/assets/javascripts/private_message/private_message_list.js
@@ -1,8 +1,12 @@
 $(function(){
   $('#private-from').on('click', function() {
     $('.private-from-list').toggle("slow");
-  })
+  });
   $('#private-to').on('click', function() {
     $('.private-to-list').toggle("slow");
-  })
+  });
+
+  $('#return-button').on('click', function(){
+    $('.private__message__return').show("slideDown");
+  });
 })
