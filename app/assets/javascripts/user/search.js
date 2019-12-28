b919b04f17eeb1a32c5fc7dc8ba4b42d0bdd13e3
yuki_m <yakan.meroyan@gmail.com>
2019-12-29 02:16:37 +0900

    Update search.js

--- app/assets/javascripts/user/search.js
+++ app/assets/javascripts/user/search.js
@@ -1,7 +1,8 @@
 $(function(){
   var user_search_list = $('#form__search__user__result')
 
-  function appendSearchUser(user){
+  function appendFollowerUser(user){
+    console.log(user)
     var html = `<div class="users__list">
                   <div class="users__list__image">
                     <img class="form__search__image" src="${user.user_image.url}">
@@ -9,9 +10,29 @@ $(function(){
                   <div class="users__list__name">
                     <a href="/users/${user.user_id}">${user.user_name}</a>
                   </div>
-                  <div class="users__list__link">
-                    <a href="/">追加</a>
+                  <form class="edit_relationship" id="edit_relationship_${user.user_follower}" action="/relationships/${user.user_follower}" accept-charset="UTF-8" data-remote="true" method="post">
+                    <input name="utf8" type="hidden" value="✓">
+                    <input type="hidden" name="_method" value="delete">
+                    <input type="submit" name="commit" value="フォローを外す" class="btn btn-large" data-disable-with="Unfollow">
+                  </form>
+                </div>
+                `
+    user_search_list.append(html);
+  }
+
+  function appendFollowedUser(user){
+    var html = `<div class="users__list">
+                  <div class="users__list__image">
+                    <img class="form__search__image" src="${user.user_image.url}">
                   </div>
+                  <div class="users__list__name">
+                    <a href="/users/${user.user_id}">${user.user_name}</a>
+                  </div>
+                  <form class="new_relationship" id="new_relationship" action="/relationships" accept-charset="UTF-8" data-remote="true" method="post">
+                    <input name="utf8" type="hidden" value="✓"><div>
+                    <input type="hidden" value=${user.user_id} name="relationship[followed_id]" id="relationship_followed_id">
+                    <input type="submit" name="commit" value="フォローをする" class="btn btn-large btn-primary" data-disable-with="Follow">
+                  </form>
                 </div>
                 `
     user_search_list.append(html);
@@ -29,6 +50,7 @@ $(function(){
 
   $('#search__user__field').on('keyup', function(){
     var input = $("#search__user__field").val();
+    var myUserId = $("#search__user__field").data('my_id');
     if (input.length !== 0) {
       $.ajax({
       type: 'GET',
@@ -43,7 +65,16 @@ $(function(){
         $('#form__search__user__result').empty();
         if (users.length !== 0) {
           users.forEach(function(user){
-            appendSearchUser(user);
+            var result = user.user_follower.filter( function( value ) {
+              if (value['id'] == myUserId) {
+                return value;
+              };
+            })
+            if (result.length != 0){
+              appendFollowerUser(user);
+            } else {
+              appendFollowedUser(user);
+            }
           });
         }
         else {
