dd502fc815854584cf5eec6c5933f3ec03791577
yuki_m <yakan.meroyan@gmail.com>
2019-12-29 16:08:02 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,21 +19,24 @@ class User < ApplicationRecord
   has_many :user     , class_name: 'private_message' , foreign_key: 'user_id'     , dependent: :destroy
   has_many :send_user, class_name: 'private_message' , foreign_key: 'send_user_id', dependent: :destroy
 
-  has_many :followed_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
-  has_many :followeds, through: :followed_relationships, source: :followed
+  acts_as_followable
+  acts_as_follower
 
-  has_many :follower_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
-  has_many :followers, through: :follower_relationships, source: :follower
+  # has_many :followed_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  # has_many :followeds, through: :followed_relationships, source: :followed
 
-  def followed?(other_user)
-    followed_relationships.find_by(followed_id: other_user.id)
-  end
+  # has_many :follower_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  # has_many :followers, through: :follower_relationships, source: :follower
 
-  def follow!(other_user)
-    followed_relationships.create!(followed_id: other_user.id)
-  end
+  # def followed?(other_user)
+  #   followed_relationships.find_by(followed_id: other_user.id)
+  # end
 
-  def unfollow!(other_user)
-    followed_relationships.find_by(followed_id: other_user.id).destroy
-  end
+  # def follow!(other_user)
+  #   followed_relationships.create!(followed_id: other_user.id)
+  # end
+
+  # def unfollow!(other_user)
+  #   followed_relationships.find_by(followed_id: other_user.id).destroy
+  # end
 end
