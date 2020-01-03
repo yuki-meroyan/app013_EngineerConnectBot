50baeb59f7690c65f50ade0197b3dbbc57dc71cd
yuki_m <yakan.meroyan@gmail.com>
2020-01-03 15:54:47 +0900

    Update beginner_comments_controller.rb

--- app/controllers/beginner_comments_controller.rb
+++ app/controllers/beginner_comments_controller.rb
@@ -1,6 +1,7 @@
 class BeginnerCommentsController < ApplicationController
-  before_action :comment_params, only: [:create]
-  before_action :set_beginner  , only: [:create]
+  before_action :user_signed_check, only: [:create]
+  before_action :comment_params   , only: [:create]
+  before_action :set_beginner     , only: [:create]
 
   def index
   end
